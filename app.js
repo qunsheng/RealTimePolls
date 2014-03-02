33ec09c45c48ae69e3db56f512aa885d8afdf481
qunsheng <qunsheng_chen@hotmail.com>
2014-03-02 18:50:14 -0500

    added more comments

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-
+// import core modules and local module
 var express = require('express');
 var routes = require('./routes');
 var http = require('http');
@@ -8,14 +8,21 @@ var app = express();
 var server = http.createServer(app);
 var io = require('socket.io').listen(server);
 
+// set variables
 app.set('port', process.env.VCAP_APP_PORT || 3000);
+// let express know where is the views diectory
+// _dirname is current directory
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
+// use middlewares
+// middleware is one step in the pipe line of processing request
 app.use(express.favicon());
 app.use(express.logger('dev'));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
+// for public folder, match the request to files directly
+// same as the legacy web server do
 app.use(express.static(path.join(__dirname, 'public')));
 
 // Handle Errors gracefully
